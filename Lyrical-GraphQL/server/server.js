47c0e9439de123396a6c7f9242155c870805f066
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2022-12-09 14:08:51 +0900

    Lyrical-GraphQL: add starter

---
+++ Lyrical-GraphQL/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const models = require('./models');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// Replace with your mongoLab URI
+const MONGO_URI = `mongodb+srv://ken:Lyrical@cluster0.2zcfsnk.mongodb.net/?retryWrites=true&w=majority`;
+
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoDB URI!');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoDB instance.'))
+  .on('error', error => console.log('Error connecting to MongoDB instance:', error));
+
+app.use(express.json());
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
